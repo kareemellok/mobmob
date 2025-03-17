4a92ea0d9722b4030d5687d08c6dce3b1b6d85c8
kareem <kareem.fahmy.a@gmail.com>
2025-03-17 16:37:47 +0200

    just testing hello world

--- src/app.js
+++ src/app.js
@@ -15,6 +15,9 @@ job.start()
 
 app.use("/api/auth", authRoutes);
 app.use("/api/auth", bookRoutes);
+app.get("/", (res) => {
+    res.send("Hello World");
+})
 
 app.use(cors());
 app.use(express.json());
